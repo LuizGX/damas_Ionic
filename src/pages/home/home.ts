322563a7bb94b246afec840e086e411c7146b9a0
LuizGX <luizxvr@hotmail.com>
2019-07-21 10:02:53 -0300

    second commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,23 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  board: any = [];
+  test: any = "assets/imgs/black.png";
   constructor(public navCtrl: NavController) {
-
+    console.log("board", this.board);
+    for(let i=0; i < 8; i++){
+      this.board[i] = [];
+      for(var j: number = 0; j< 8; j++) {
+        if(i%2 == 0 && j%2 == 0){
+          this.board[i][j] = "black";
+        }else if(i%2 != 0 && j%2 != 0){
+          this.board[i][j] = "black"
+        } else{
+          this.board[i][j] = "white"
+        }
+      }
+    }
+    console.log(this.board);
   }
 
 }
